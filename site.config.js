052bae0e8f7419c6544dc24e64ff4ec077132bd8
Khoan-IT <duckhoan.ds@gmail.com>
2024-07-18 09:34:44 +0700

    update config for deploying

--- site.config.js
+++ site.config.js
@@ -18,7 +18,7 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
+    title: "DucKhoan-Blog",
     description: "welcome to morethan-log!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
