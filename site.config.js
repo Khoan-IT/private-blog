5dfb71f668bd2c205be115867cad33798508983a
Khoan-IT <duckhoan.ds@gmail.com>
2024-07-22 12:01:03 +0700

    update avatar

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Duc Khoan",
-    image: "/avatar.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "AI Researcher",
     bio: "Bring AI to the World",
     email: "duckhoan.work@gmail.com",
